9aa961e72837d3d763ad2bf73595f49125d0f31a
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-12 18:25:38 +0000

    Modificaciones en C9

--- index.php
+++ index.php
@@ -1,44 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title><?php wp_title(); ?></title>
-
     <!-- Bootstrap Core CSS -->
-    <link href="<?php bloginfo('template_url');?>/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="<?php bloginfo('template_url');?>/css/agency.css" rel="stylesheet">
-    <!-- Estilos CSS -->
-     <link rel="StyleSheet" href="<?php bloginfo('template_url');?>/estilos.css" type="text/css">
+    <link href="<?php bloginfo('template_url');?>/css/main.less" rel="stylesheet">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.0/less.min.js"></script>
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-
+    <link href='' rel='stylesheet' type='text/css'>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
-  
-    
 </head>
-
-<body id="page-top" class="index">
+<body id  ="page-top" class="index">
 <div class="contenedor">
-    
     <!-- Navigation -->
     <div class="row">
             <nav class=" navbar navbar-default navbar-fixed-top">
@@ -287,7 +270,6 @@
                         
         });
 
-        
 
     });
     </script>

fc6d9372c8114010cb6a3663287a43155686e506
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-12 21:45:24 +0000

    eimiar pk_title title header

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title><?php pk_title(); ?></title>
+    <title><?php //pk_title(); ?></title>
     <!-- Bootstrap Core CSS -->
     <link href="<?php bloginfo('template_url');?>/css/main.less" rel="stylesheet/less">
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.0/less.min.js"></script>

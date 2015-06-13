cb182673cb37149474c4d34fcf4fad0e730d5f37
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-13 20:05:02 +0000

    agregando la seccion contacto

--- index.php
+++ index.php
@@ -318,14 +318,65 @@ if(!empty($pagename)){
                 </div>
             </div>
         </div>
-<div class="container">
-<div class="row">
-<div class="col-lg-12">
-<input type="submit" class="btn btn-success btn-center" value="IR AL BLOG">
-                
-            </div>
+ <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <input type="submit" class="btn btn-success btn-center boton" value="IR AL BLOG">
+            <h3 class="section-subheading subtitu">Lorem ipsum ES SIMPLEMTE EL TEXTO DE</h3>
+            <h3 class="section-subheading subtitu1">RELLENO DE LAS IMPRENTAS Y ARCHIVOS DE RELLENO.</h3>
         </div>
     </div>
+</div>
+    </section>
+    
+     <!-- Contact Section -->
+    <section id="contacto">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Contactos</h2>
+                    <h3 class="section-subheading subtitu2">Lorem ipsum ES SIMPLEMTE EL TEXTO DE</h3>
+                    <h3 class="section-subheading subtitu3">RELLENO DE LAS IMPRENTAS Y ARCHIVOS DE RELLENO.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <h3 class="subtitu4">NOMBRE Y APELLIDO</h3>
+                                    <input type="text" class="form-control" placeholder="Nombre y apellido *" id="name" required data-validation-required-message="Please enter your name.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <h3 class="subtitu4">EMAIL</h3>
+                                    <input type="email" class="form-control" placeholder=" Email *" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                       <h3 class="subtitu4">TELÉFONO </h3>
+                                    <input type="tel" class="form-control" placeholder="Teléfono *" id="phone" required data-validation-required-message="Please enter your phone number.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                     <h3 class="subtitu4">MENSAJE</h3>
+                                    <textarea class="form-control" placeholder="Mensaje*" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div>
+                                <button type="submit" class="btn btn-xl">ENVIAR</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </section>
 
     <!-- jQuery -->

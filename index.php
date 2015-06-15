d9c6e0569e47b2278f8fa1d0b7c16c99a586ccde
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-15 17:33:09 +0000

    fondo

--- index.php
+++ index.php
@@ -400,6 +400,7 @@ if(!empty($pagename)){
         <div class="mapa-container ">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7881.385361790559!2d-79.53253804026551!3d9.000399793436857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca85c76ab2d47%3A0xea6ae22d50fbc8cd!2sEdificio+Century+Tower!5e0!3m2!1sen!2s!4v1434382131796" width="600" height="450" frameborder="0" style="border:0"></iframe>        
         </div>
+    </section>
     <footer>
         <div class="container">
             <div class="row">
@@ -426,7 +427,6 @@ if(!empty($pagename)){
             </div>
         </div>
     </footer>
-    </section>
 
     <!-- jQuery -->
 <script src="<?php bloginfo('template_url');?>/js/jquery.js"></script>

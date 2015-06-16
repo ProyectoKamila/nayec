21a2594065014547388602cd517e35c252260e64
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-16 20:03:19 +0000

    mapa

--- index.php
+++ index.php
@@ -391,6 +391,13 @@ if(!empty($pagename)){
         </div>
     </div>
 </div>
+    <div class="container">
+            <div class="col-lg-12 alto-mapa">
+         <div class="mapa-container ">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7881.385361790559!2d-79.53253804026551!3d9.000399793436857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca85c76ab2d47%3A0xea6ae22d50fbc8cd!2sEdificio+Century+Tower!5e0!3m2!1sen!2s!4v1434382131796" width="600" height="450" frameborder="0" style="border:0"></iframe>        
+        </div>
+        </div>
+    </div>
     </section>
     
     <footer >

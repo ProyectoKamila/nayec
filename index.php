b5c3ff610aba83634f58dcce9c4fd53b0aba181c
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-17 20:50:20 +0000

    mapa

--- index.php
+++ index.php
@@ -501,7 +501,7 @@ if(!empty($pagename)){
         if( !empty($location) ):
     ?>
 <div class="acf-map">
-	<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+	<div class="marker" <?php echo $location['google-map']; ?>></div>
 </div>
 <?php endif; ?>
         </div>

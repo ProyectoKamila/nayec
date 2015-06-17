5a43354f816cea99d89ff2f108b55c36b9ab7c4a
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-17 15:57:13 +0000

    formulario

--- index.php
+++ index.php
@@ -438,8 +438,8 @@ if(!empty($pagename)){
     <section id="contacto">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading"><?php the_title();?></h2>
+                <div class="col-lg-12 text-center formu">
+                    <h2 class="section-heading contacto-emca"><?php the_title();?></h2>
                        <?php if(get_field('contacto-texto1')){?>
                     <h3 class="section-subheading subtitu2"><?php the_field('contacto-texto1');?></h3>
                      <?php } ?>

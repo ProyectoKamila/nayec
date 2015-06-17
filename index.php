a0c40674ce96e85d29486851e58218e737f9e96c
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-17 21:11:37 +0000

    mapa

--- index.php
+++ index.php
@@ -497,7 +497,7 @@ if(!empty($pagename)){
             <div class="col-lg-12 alto-mapa">
          <div class="mapa-container">
          <?php if(get_field('google_map')){?>
-              <?php the_field('google_map');?> 
+             <iframe src=" <?php the_field('google_map');?>" width="600" height="450" frameborder="0" style="border:0"></iframe>
          
           <?php } ?>
 

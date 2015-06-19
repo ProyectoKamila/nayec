7ba4e9daf8507c957855d4664ac8e25a8dcca3c6
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-19 15:35:43 +0000

    wp

--- index.php
+++ index.php
@@ -82,11 +82,11 @@
     <header >
 <div id="wrapper01">
 <ul id="slider1">
-<?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+   
             
-<li style="background:url(<?php echo '$feat_image';?>)  top center no-repeat; background-size:cover; sin-padding">
-      
-    <div class="cuadro-slider">
+<li style="background: url(<?php bloginfo('template_url');?>/slider/demos/images/ <?php the_field('imagen1');?> ) top center no-repeat; background-size:cover; sin-padding">
+    <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+    <div class="cuadro-slider" style="background:url(<?php echo '$feat_image';?>)">
         <h2 class="slider-titulo1">UN ALIADO ESTRATÉGICO</h2>
         <p class="slider-parrafo">
             LOREM IPSUM ES SIMPLEMENTE EL TEXTO DE RELLENO 

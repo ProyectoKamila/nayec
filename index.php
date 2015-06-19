ce153350e3726aec1951c771c953a2e1f14bde8e
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-19 16:41:01 +0000

    wp

--- index.php
+++ index.php
@@ -85,7 +85,8 @@
    
             
 <li style="background: url(<?php bloginfo('template_url');?>/slider/demos/images/barco.jpg ) top center no-repeat; background-size:cover; sin-padding">
-    <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>
+    <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+    echo '$feat_image';?>
     <div class="cuadro-slider" style="background:url(<?php echo '$feat_image';?>)">
         <h2 class="slider-titulo1">UN ALIADO ESTRATÉGICO</h2>
         <p class="slider-parrafo">

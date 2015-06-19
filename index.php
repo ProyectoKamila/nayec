f2fa4acfd041815c1e0f9433eb358789d6aaffec
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-19 17:29:20 +0000

    wp

--- index.php
+++ index.php
@@ -80,29 +80,26 @@
             </nav>
     </div>
     <header >
-<div id="wrapper01">
-<ul id="slider1">
-   
-   <?php $pagename = query_posts(array('post_type'=>'portada','pagename'=>'','posts_per_page'=>1)); ?>
- <?php if(!empty($pagename)) {?>
-     <?php while(have_posts()){ the_post();?>    
-<li style="background: url(<?php bloginfo('template_url');?>/slider/demos/images/barco.jpg ) top center no-repeat; background-size:cover; sin-padding">
-    
-    <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>
-    <div class="cuadro-slider" style="background:url(<?php echo '$feat_image';?>)">
-        <h2 class="slider-titulo1">UN ALIADO ESTRATÉGICO</h2>
-        <p class="slider-parrafo">
-            LOREM IPSUM ES SIMPLEMENTE EL TEXTO DE RELLENO 
-            DE LAS IMPRENTAS Y ARCHIVOS DE TEXTO. LOREM 
-            IPSUM HA SIDO EL TEXTO DE 
-            RELLENO ESTÁNDAR DE LAS INDUSTRIAS DESDE EL AÑO 1500.
-        </p>
-        <button type="button" class="btn btn-primary slider-boton">VER MÁS</button>
-    </div>
+<?php $pagename = query_posts(array('post_type'=>'portada','pagename'=>'','posts_per_page'=>1)); ?>
+    <?php if(!empty($pagename)) {?>
+         <?php while(have_posts()){ the_post();?>
+        <div id="wrapper01">
+            <ul id="slider1">
+                <li style="background: url(<?php bloginfo('template_url');?>/slider/demos/images/barco.jpg ) top center no-repeat; background-size:cover; sin-padding">
+                <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>
+                    <div class="cuadro-slider" style="background:url(<?php echo '$feat_image';?>)">
+                        <h2 class="slider-titulo1">UN ALIADO ESTRATÉGICO</h2>
+                        <p class="slider-parrafo">
+                            LOREM IPSUM ES SIMPLEMENTE EL TEXTO DE RELLENO 
+                            DE LAS IMPRENTAS Y ARCHIVOS DE TEXTO. LOREM 
+                            IPSUM HA SIDO EL TEXTO DE 
+                            RELLENO ESTÁNDAR DE LAS INDUSTRIAS DESDE EL AÑO 1500.
+                        </p>
+                        <button type="button" class="btn btn-primary slider-boton">VER MÁS</button>
+                    </div>
                      
                     </li>
-                     <?php } ?>
-                      <?php } ?>
+                     
                     
 <li style="background: url(<?php bloginfo('template_url');?>/slider/demos/images/barco.jpg) top center no-repeat; background-size:cover;">
  <div class="cuadro-slider">
@@ -146,6 +143,8 @@
                 </li>
             </ul>
         </div>
+    <?php } ?>
+<?php } ?>
     </header>
               
     <!-- Services Section -->

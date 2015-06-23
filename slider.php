f12bf4634baa96041c90568592ae9e65ff57cdc5
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-23 18:59:35 +0000

    maqueta

--- slider.php
+++ slider.php
@@ -11,7 +11,7 @@
                                 <li style="background: url(<?php echo $feat_image;?> ) top center no-repeat; background-size:cover; sin-padding">
                                     <div class="container">
                                         <div class="row">
-                                           <div class="col-lg-6 col-lg-offset-3">
+                                           <div class="col-lg-6 col-lg-offset-5">
                                                <div class="cuadro-slider">
                                                      <?php if(get_field('titulo-fondo')){?>
                                                         <h2 class="slider-titulo1"><?php the_field('titulo-fondo');?> </h2>

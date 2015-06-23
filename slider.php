47d1e41d0eb24742252a81993e513ad4e2bde3fa
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-23 15:57:47 +0000

    maqueta

--- slider.php
+++ slider.php
@@ -1,40 +1,36 @@
 <?php $pagename = query_posts(array('post_type'=>'portada',''=>'','posts_per_page'=>4)); ?>
     <?php if(!empty($pagename)) {?>
        
-    <header >
-       <div class="container-fluid sin-padding">
-           <div class="row">
-        <div class="col-xs-12">
-            <ul id="slider1">
-                 <?php while(have_posts()){ the_post();?>
-                  <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>
-                <li style="background: url(<?php echo $feat_image;?> ) top center no-repeat; background-size:cover; sin-padding">
-               <div class="container">
-                   <div class="row">
-                       <div class="col-lg-6 col-lg-ofset-3">
-                           <div class="cuadro-slider">
-                             <?php if(get_field('titulo-fondo')){?>
-                                <h2 class="slider-titulo1"><?php the_field('titulo-fondo');?> </h2>
-                             <?php } ?>
-                              <?php if(get_field('parrafo-fondo')){?>
-                        <p class="slider-parrafo">
-                           <?php the_field('parrafo-fondo');?> 
-                        </p>
-                         <?php } ?>
-                        <button type="button" class="btn btn-primary slider-boton">VER MÁS</button>
-                    </div>
-                       </div>
-                   </div>
-               </div>
-                    
-                </li>
-                
-             <?php } ?>
-
-            </ul>
+<header>
+    <div class="container-fluid sin-padding">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <ul id="slider1">
+                     <?php while(have_posts()){ the_post();?>
+                          <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>
+                                <li style="background: url(<?php echo $feat_image;?> ) top center no-repeat; background-size:cover; sin-padding">
+                                    <div class="container">
+                                        <div class="row">
+                                           <div class="col-lg-6 col-lg-offset-3">
+                                               <div class="cuadro-slider">
+                                                     <?php if(get_field('titulo-fondo')){?>
+                                                        <h2 class="slider-titulo1"><?php the_field('titulo-fondo');?> </h2>
+                                                     <?php } ?>
+                                                      <?php if(get_field('parrafo-fondo')){?>
+                                                            <p class="slider-parrafo">
+                                                              <?php the_field('parrafo-fondo');?> 
+                                                            </p>
+                                                     <?php } ?>
+                                                    <button type="button" class="btn btn-primary slider-boton">VER MÁS</button>
+                                                </div>
+                                           </div>
+                                       </div>
+                                   </div>
+                                </li>
+                     <?php } ?>
+                </ul>
+            </div>
         </div>
-        
-        </div>
-       </div>
-    </header>
+    </div>
+</header>
 <?php } ?>     

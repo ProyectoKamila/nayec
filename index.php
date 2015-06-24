bb4346000736f58e2d07167cb42066973ba485c6
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-24 14:15:13 +0000

    maqueta

--- index.php
+++ index.php
@@ -544,7 +544,7 @@ if(!empty($pagename)){
                        
                     </div>
                     <?php } ?>
-                <?php } ?>
+                <?php } wp_reset_query(); ?> 
                     <div class="col-md-3 col-sm-6">
                      <div class="row">
                                 <div class="col-lg-4 col-md-4 col-sm-12 logo-ubicacion  ">
@@ -561,7 +561,7 @@ if(!empty($pagename)){
                     </div>
                 </div>
          <?php } ?>
-        <?php } ?>
+        <?php } wp_reset_query(); ?> 
                 <div class="row">
                     <div class="col-lg-12 text-center pk">
                         <p  class="desarrollado-pk">Copyright © 2015 www.nayec.com Todos los derechos reservados

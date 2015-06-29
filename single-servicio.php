98c36181c490b529ac4841ae0f4e6b0f90349c6d
Jose Vivas <jose.vivas@proyectokamila.com>
2015-06-29 18:44:10 +0000

    single servicios

--- single-servicio.php
+++ single-servicio.php
@@ -121,7 +121,6 @@ if(!empty($pagename)){ ?>
                     <div class="row sin-padding">
                         <?php if(get_field('background_left')){?>
                     <div class="aduanas-n sin-padding col-xs-12 col-sm-12 col-md-6  col-lg-6  "style="background: url(<?php the_field('background_left'); ?>) top center no-repeat; backgroun-size:cover;">
-                    </div>
                         <?php }else{ ?>
                     <div class="aduanas sin-padding col-xs-12 col-sm-12 col-md-6  col-lg-6  ">
                         <?php } ?>
@@ -136,7 +135,7 @@ if(!empty($pagename)){ ?>
                     </div>
                     <?php if(get_field('background_right')) { ?>
                     <div class="logistica-n sin-padding col-xs-12 col-sm-12 col-md-6  col-lg-6" style="background:url(<?php the_field('background_right');?>) top center no-repeat; background-size:cover;">
-                    </div>
+                    
                     <?php } else {?>
                         <div class="logistica sin-padding col-xs-12 col-sm-12 col-md-6  col-lg-6" >
                     <?php } ?>
@@ -152,7 +151,7 @@ if(!empty($pagename)){ ?>
                                         </div>
                                         </div>
                                     <?php } ?>                
-                    </div>
+                   
 <?php } ?>
  <!-- Contact Section -->
  <?php $pagename = query_posts(array('post_type'=>'page','pagename'=>'contacto','posts_per_page'=>1)); ?>
